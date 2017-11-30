ebd5e93f46382a0add8d16232a71e27ee9a9755d
Elliot Dill <P14137255@LEC-ADMIN.dmu.ac.uk>
2017-11-30 16:39:24 +0000

    Added responsive design to page2. Need to implement this on all pages once completely satisfied with new css design. Will also need to remove soon to be duplicate css file.

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+"use strict";
+(function() {
+  let menuButton = document.getElementById("menu");
+  let navMenu = document.getElementById("nav-menu");
+  
+  menuButton.addEventListener("click", toggleMenu);
+  
+  let toggle = false;
+  function toggleMenu() {
+    if (toggle) {
+      navMenu.classList.remove("show-menu"),
+      toggle = false
+    }
+    else {
+      navMenu.classList.add("show-menu"),
+      toggle = true
+    }
+  }
+  
+})();
