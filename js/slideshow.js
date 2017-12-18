82cf58faf10b93e00cc6d502a511f2822b2479fe
BuildTools <unconfigured@null.spigotmc.org>
2017-12-18 19:48:55 +0000

    Added flexbox images, will link to relevant news articles. Updated some class and ID values and other technical details to follow standards shown in sites on readme

--- js/slideshow.js
+++ js/slideshow.js
@@ -16,7 +16,7 @@ function showSlides(n) {
   let index;
   // Variables for HTML elements
   let slides = document.getElementsByClassName("slides");
-  let dots = document.getElementsByClassName("dot");
+  let selectors = document.getElementsByClassName("selector");
   // If gone forwards from 1, go to first slide
   if (n > slides.length) {slideIndex = 1}
   // If gone backwards from 1, go to final slide
@@ -25,12 +25,12 @@ function showSlides(n) {
   for (index = 0; index < slides.length; index++) {
       slides[index].style.display = "none";
   }
-  // Loop through the dots and set them all as inactive to stop all dots becoming active as slideshow progresses
-  for (index = 0; index < dots.length; index++) {
-      dots[index].className = dots[index].className.replace(" active", "");
+  // Loop through the selectors and set them all as inactive to stop all selectors becoming active as slideshow progresses
+  for (index = 0; index < selectors.length; index++) {
+    selectors[index].className = selectors[index].className.replace(" active", "");
   }
   // Show current slide
   slides[slideIndex-1].style.display = "block";
-  // Set active "dot"
-  dots[slideIndex-1].className += " active";
+  // Set active "selector"
+  selectors[slideIndex-1].className += " active";
 }
--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
-# This is my ReadMe for Front-End Web Development
+# Front-End Web Development Assignment 2 Website
+
+## Style Guide/Standard
+https://google.github.io/styleguide/htmlcssguide.html (up to shorthand)
+https://html.spec.whatwg.org/multipage/syntax.html#syntax-tag-omission
