dc479029521ab8480b7331321b9020ee8f996213
elliot4959 <elliot4959@live.co.uk>
2017-12-22 12:39:34 +0000

    Added API call to weather service

---
+++ js/api.js
@@ -0,0 +1,34 @@
+"use strict";
+
+(function () {
+  // creates a new object called xhr
+  // this will handle the API call
+  let xhr = new XMLHttpRequest();
+  // get elements to print data
+  let tempOutput = document.getElementById("track-temp");
+  
+  // construct base for request URL
+  let baseURL = "https://api.worldweatheronline.com/premium/v1/weather.ashx?key=6e5bf6c426e3464d831142025172411&q=Silverstone,UnitedKingdom&num_of_days=1&fx=no&mca=no&format=json";
+
+  function gatherData(data) {
+    // Get the data from the API call
+    let currentCondition = data.data.current_condition[0];
+    console.log(currentCondition);
+    tempOutput.innerHTML = currentCondition.temp_C + "ºC";
+  }
+
+  // open a connection to the API
+  xhr.open("GET", baseURL, true);
+  // send the request
+  xhr.send();
+  // if response OK, handle data
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      // parse the JSON
+      let response = JSON.parse(xhr.responseText);
+      // deal with parsed JSON data
+      gatherData(response);
+    }
+  };
+
+}());
